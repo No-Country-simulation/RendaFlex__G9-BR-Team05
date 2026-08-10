39fd49b7f9197d8bff31623a7c0a8f92dd0ecb0d
Raul Douglas <rauldouglasoliveira@gmail.com>
2026-08-10 15:02:44 -0300

    feat(backend): add transaction classification HTTP client

--- src/main/java/com/rendaflex/demo/dto/internal/InternalClassificationTransaction.java
+++ src/main/java/com/rendaflex/demo/dto/internal/InternalClassificationTransaction.java
@@ -1,7 +1,10 @@
 package com.rendaflex.demo.dto.internal;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.math.BigDecimal;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record InternalClassificationTransaction(
         Integer sourceIndex,
         String description,
